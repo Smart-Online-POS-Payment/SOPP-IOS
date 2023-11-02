e7c1625a9fe4c8cd3817ae8a7e0801d09fbf893f
omergguler <oguler19@ku.edu.tr>
2023-11-02 20:46:02 +0300

    Add ProfileView

---
+++ SOPPIOS/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SOPPIOS
+//
+//  Created by Omer Guler on 2/11/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
