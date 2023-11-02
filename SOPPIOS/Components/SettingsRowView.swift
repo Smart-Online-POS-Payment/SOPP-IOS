55d4ffe7c3a308ae7c45529d5a0e64a89ed81119
omergguler <oguler19@ku.edu.tr>
2023-11-02 20:47:34 +0300

    Add ProfileView

--- SOPPIOS/Components/SettingsRowView.swift
+++ SOPPIOS/Components/SettingsRowView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct SettingsRowView: View {
+    let imageName: String
+    let title: String
+    let tintColor: Color
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 12) {
+            Image(systemName: imageName)
+                .imageScale(.small)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(tintColor)
+            
+            Text(title)
+                .font(.subheadline)
+                .foregroundStyle(Color.black)
+            
+        }
     }
 }
 
-#Preview {
-    SettingsRowView()
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+    }
 }
+
--- SOPPIOS/Core/Profile/ProfileView.swift
+++ SOPPIOS/Core/Profile/ProfileView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                HStack {
+                    Text("OG")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(Color.white)
+                        .frame(width: 72, height: 72)
+                        .background(Color(.systemGray3))
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    VStack {
+                        Text("test@gmail.com")
+                            .fontWeight(.semibold)
+                            .padding(.top, 4)
+                            .accentColor(.gray)
+                    }
+                }
+                
+            }
+            
+            Section("General") {
+                SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+            }
+            
+            Section("Account") {
+                Button {
+                    print("Sign out...")
+                } label: {
+                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
+                }
+                
+                Button {
+                    print("Delete account...")
+                } label: {
+                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                }
+            }
+        }
     }
 }
 
