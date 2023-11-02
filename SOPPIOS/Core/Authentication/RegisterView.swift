5de1036afe28771de86a68e21a6ecdd77c774c43
omergguler <oguler19@ku.edu.tr>
2023-11-02 19:31:58 +0300

    Add RegisterView

---
+++ SOPPIOS/Core/Authentication/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  SOPPIOS
+//
+//  Created by Omer Guler on 2/11/2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
