d6d9ab061bd57f036740303c2a58c0bca9f30e32
omergguler <oguler19@ku.edu.tr>
2023-12-10 21:13:42 +0300

    Implement main screen

---
+++ SOPPIOS/Core/Authentication/View/UploadMoneyView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadMoneyView.swift
+//  SOPPIOS
+//
+//  Created by Omer Guler on 10/12/2023.
+//
+
+import SwiftUI
+
+struct UploadMoneyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadMoneyView()
+}
