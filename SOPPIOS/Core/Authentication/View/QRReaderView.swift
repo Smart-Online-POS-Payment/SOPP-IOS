f4ae7a3bcfd2ac061fff0efb21488a265f1a32ad
omergguler <oguler19@ku.edu.tr>
2023-12-12 16:42:02 +0300

    Add QR Reader

--- SOPPIOS/Core/Authentication/View/QRReaderView.swift
+++ SOPPIOS/Core/Authentication/View/QRReaderView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import CodeScanner
+
 
 struct QRReaderView: View {
-    var body: some View {
-        Text("QR Reader View")
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Scan a QR code to get started"
+    
+    
+    var scannerSheet: some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
     }
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            
+            Button("Scan QR Code") {
+                self.isPresentingScanner = true
+            }
+            
+            .sheet(isPresented: $isPresentingScanner){
+                self.scannerSheet
+            }
+        }
+    }
+    
+    
 }
 
 #Preview {
